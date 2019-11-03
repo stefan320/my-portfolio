8a744d15d413cb971f9b0a8e5dd29248f94876d7
stefan320 <stefan_014_@hotmail.com>
2019-11-03 13:18:12 +0100

    My portfolio first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const headerLogo = document.querySelector(".header--logo");
+const headerEnter = document.querySelector(".header__animation--enter");
+const headerLink = document.querySelector(".header__animation--link");
+const animateLineY = document.querySelector(".header__animation--lineY");
+const animateLineX = document.querySelector(".header__animation--lineX");
+
+//Events
+window.addEventListener("load", () => {
+  // Prevent animations to run before load
+  body.classList.remove("preload");
+  animateLineY.style.transform = "scaleY(75)";
+  animateLineX.style.transform = "scaleX(75)";
+  headerEnter.style.opacity = "1";
+  headerLink.style.opacity = "1";
+});
