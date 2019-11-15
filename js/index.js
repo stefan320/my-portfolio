790f02512e7ce816801aff2497d749e93c7d1121
stefan320 <stefan_014_@hotmail.com>
2019-11-15 19:38:52 +0100

    Added Contact Section fixed part of the layout spaceing & added JS to be able to close the navigation

--- js/index.js
+++ js/index.js
@@ -4,13 +4,20 @@ const headerEnter = document.querySelector(".header__animation--enter");
 const headerLink = document.querySelector(".header__animation--link");
 const animateLineY = document.querySelector(".header__animation--lineY");
 const animateLineX = document.querySelector(".header__animation--lineX");
+const navCheckbox = document.querySelector(".navigation--toggle");
+const navList = document.querySelector(".navigation__list");
 
 // Events
 window.addEventListener("load", () => {
   // Prevent animations to run before load
   body.classList.remove("preload");
-  animateLineY.style.transform = "scaleY(51)";
-  animateLineX.style.transform = "scaleX(51)";
+  animateLineY.style.transform = "scaleY(38)";
+  animateLineX.style.transform = "scaleX(70)";
   headerEnter.style.opacity = "1";
   headerLink.style.opacity = "1";
+});
+
+// NAVIGATION
+navList.addEventListener("click", () => {
+  navCheckbox.checked = false; //Uncheck the checkbox so the navigation close
 });
