e79958d0b7fe34df696a2b6279ec437ed030b796
stefan320 <stefan_014_@hotmail.com>
2019-11-27 19:25:52 +0100

    replaced header background with svg to add animation on line hover and also keep a consistent distance between each line on all screen sizes

---
+++ js/canvas.js
@@ -0,0 +1,110 @@
+const canvas = document.querySelector(".header--canvas");
+const logo = document.querySelector(".logo");
+const c = canvas.getContext("2d");
+
+const defaultColor = "rgba(255,255,255, .4)";
+const whiteGold = " #bba58e";
+const defaultLineWidth = 0.6;
+let mtx = 30; //MoveTo X Starting Value
+let lty = 30; //Linto To Y Starting Value
+
+const mouse = {
+  x: undefined,
+  y: undefined
+};
+const linesArray = [];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//Events
+canvas.addEventListener("mousemove", e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+canvas.addEventListener("mouseout", e => {
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+class Line {
+  constructor(mtx, mty, ltx, lty, strokeStyle, lineWidth = defaultLineWidth) {
+    this.mtx = mtx;
+    this.mty = mty;
+    this.ltx = ltx;
+    this.lty = lty;
+    this.strokeStyle = strokeStyle;
+    this.lineWidth = lineWidth;
+  }
+
+  drawLine() {
+    c.beginPath();
+    c.moveTo(this.mtx, this.mty);
+    c.lineTo(this.ltx, this.lty);
+    c.lineWidth = this.lineWidth;
+    c.strokeStyle = this.strokeStyle;
+    c.stroke();
+  }
+
+  update() {
+    if (
+      mouse.x + mouse.y >= this.mtx - 10 &&
+      mouse.x + mouse.y <= this.mtx + 10
+    ) {
+      this.lineWidth = 2;
+      this.strokeStyle = whiteGold;
+    } else {
+      this.strokeStyle = defaultColor;
+      this.lineWidth = defaultLineWidth;
+    }
+  }
+}
+
+const createLines = () => {
+  for (let i = 0; i < 300; i++) {
+    linesArray.push(new Line(mtx, 0, 0, lty, defaultColor));
+    linesArray[i].drawLine();
+    mtx += 30;
+    lty += 30;
+  }
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  linesArray.forEach(el => {
+    el.update();
+    el.drawLine();
+  });
+
+  // c.drawImage(
+  //   logo,
+  //   innerWidth / 2 - logo.width / 2,
+  //   innerHeight / 2 - logo.height / 2
+  // );
+
+  // c.moveTo(
+  //   innerWidth / 2 - logo.width / 2 + 30,
+  //   innerHeight / 2 + logo.height / 2 + 20
+  // );
+
+  // c.lineTo(
+  //   innerWidth / 2 - logo.width / 2 + 30,
+  //   innerHeight / 2 + logo.height / 2 + 190
+  // );
+
+  // c.lineTo(
+  //   innerWidth / 2 + logo.width / 2 - 50,
+  //   innerHeight / 2 + logo.height / 2 + 190
+  // );
+
+  // c.lineWidth = 2.8;
+  // c.stroke();
+}
+createLines();
+animate();
