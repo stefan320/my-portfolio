b401f51d2d558c0e75c1860ee11c571a05ca4268
stefan320 <stefan_014_@hotmail.com>
2020-01-16 12:46:09 +0100

    Added scroll to top button

--- js/index.js
+++ js/index.js
@@ -28,6 +28,29 @@ headerLogo.onload = function() {
   }, 700);
 };
 
+// SCROLL TO TOP BUTTON
+//Get the button:
+const scrollToTopBtn = document.querySelector(".scroll-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // NAVIGATION
 navList.addEventListener("click", () => {
   navCheckbox.checked = false; //Uncheck the checkbox so the navigation close
