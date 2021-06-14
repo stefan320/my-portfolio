958da12c1c8fa6c5557f369bb4ace638935987e0
stefan320 <stefan_014_@hotmail.com>
2021-06-14 10:35:24 +0200

    addded new survey project

--- js/animations.js
+++ js/animations.js
@@ -1,24 +1,26 @@
 AOS.init({
   delay: 250,
-  duration: 500
+  duration: 500,
 });
 
 //Projects
 
 let container = document.querySelectorAll(".projects__links--container");
 const backArrows = document.querySelectorAll(".projects__details--arrow");
-const jmbOverlay = document.querySelector(".projects__overlay--jmb");
+// const jmbOverlay = document.querySelector(".projects__overlay--jmb");
 
 let projectSelection;
 
 container = Array.from(container);
 
-container.forEach(e => {
-  e.addEventListener("click", ev => {
+console.log(container);
+
+container.forEach((e) => {
+  e.addEventListener("click", (ev) => {
     projectSelection = ev.target.dataset.reference;
     document.getElementById(projectSelection).classList.add("hide");
 
-    //rotate the container to show the visit website element
+    //rotate div to show the visit website element
     ev.target.parentNode.children[0].style.transform = "rotateX(-90deg)";
     ev.target.parentNode.children[0].style.opacity = "0";
     ev.target.parentNode.children[1].style.transform = "rotateX(0deg)";
@@ -27,12 +29,12 @@ container.forEach(e => {
 });
 
 backArrows.forEach((e, index) => {
-  e.addEventListener("click", ev => {
+  e.addEventListener("click", (ev) => {
     console.log(e);
     projectSelection = ev.target.dataset.reference;
     document.getElementById(projectSelection).classList.remove("hide");
 
-    //rotate the container to show the Project Details element
+    //rotate div to show the Project Details element
     container[index].children[0].style.transform = "rotateX(0deg)";
     container[index].children[0].style.opacity = "1";
     container[index].children[1].style.transform = "rotateX(-90deg)";
